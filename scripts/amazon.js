76cf69c7de509b6a972cc4b090eb949a10e6318a
Brayan Pineda <pinedab606@gmail.com>
2024-06-06 14:02:03 -0500

    Using the products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -6,34 +6,6 @@ Steps for javascript:
 */
 
 //Save the data
-const products = [{
-        image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count: 87,
-        },
-        priceCents: 1090
-    },
-    {
-        image : 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            stars: 4.0,
-            count: 127,
-        },
-        priceCents: 2095
-    },
-    {
-        image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56,
-        },
-        priceCents: 799
-    },
-];
 
 //2 Step - Generate the HTML
 productHTML = '';
@@ -90,4 +62,6 @@ products.forEach((product, index) => {
     `;
     productHTML += html;
 });
+
+//Step 3- make it inerative
 document.querySelector(".products-grid").innerHTML = productHTML;

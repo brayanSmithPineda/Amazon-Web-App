f021dfd40a536cf8bd9b8702713249ec831f7350
Brayan Pineda <pinedab606@gmail.com>
2024-06-08 19:13:33 -0500

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -5,8 +5,8 @@ Steps for javascript:
 3- make it inerative
 */
 
-//Save the data
-
+// 1 Step - Save the data
+/*We saved the data but we decides to save it in separete file products.js so we can keep our code clean */
 //2 Step - Generate the HTML
 productHTML = '';
 products.forEach((product, index) => {
@@ -55,13 +55,46 @@ products.forEach((product, index) => {
             Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart"  
+            data-product-id = ${product.id}> 
         Add to Cart
         </button>
     </div>
-    `;
+    `; // we set a data attribute to the add to cart button that way we can identify which product to add when we click the button
     productHTML += html;
 });
 
-//Step 3- make it inerative
 document.querySelector(".products-grid").innerHTML = productHTML;
+
+
+//Step 3- make it inerative
+/*1- Save the data in a list of objects **Cart Quantity**
+2- create a funtion that listen to every click we do on the **add button**, each time we click on it we  me save into our cart variable an object with the id of the product and the quantity
+3- then we display the CartQuantity variable on the webpage
+*/
+document.querySelectorAll('.js-add-to-cart') //This is the list of the Add Buttons  
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;  // the dataset property return all the data attributes associated witht he button, when we use .productName we are returning just the name data attribute
+
+            //we first check if the item is already in our cart so we just increase the quantity, else we add it to the cart
+            let item;
+            cart.forEach((itemObject) => {
+                // console.log(itemObject);
+                if (productId === itemObject.productId){
+                    item = itemObject;
+                }
+            });
+
+            if (item){
+                item.quantity ++;
+            } else {
+                cart.push({
+                    productId: productId,
+                    quantity: 1
+                }); 
+            }
+            console.log(cart);
+        }); // we have to use the button when we add the event listernet otherwise we are going to run the code for all the products everytime we click on any button
+    }    
+);

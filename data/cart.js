81a9e4b0a403f0393a6508d337f56af286bff739
Brayan Pineda <pinedab606@gmail.com>
2024-07-06 15:48:41 -0500

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,18 @@
 //We create the cart variable, but we are going to use the data save in local storage, that way it does not dissapear every time we refresh the page
 
 //if local storage is none the we just create and empty list for the car
-export let cart =  JSON.parse(localStorage.getItem('cart')) || [];
+export let cart =  JSON.parse(localStorage.getItem('cart')) || [
+    {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1,
+        deliveryOptionId: '1'
+    },
+    {
+        productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 2,
+        deliveryOptionId: '2'
+    }
+];
 
 function saveToLocalStorage(){
     //we create this function to save the cart data into local storage, that way we could mantain the data even if you refresh the page, if we do not do this, then when changing from the amazon main page to the checkout the product items are not going to appear
@@ -24,7 +35,8 @@ export function addToCart(productId){
     } else {
         cart.push({
             productId: productId,
-            quantity: 1
+            quantity: 1,
+            deliveryOptionId: '1'
         }); 
     };
 
@@ -32,7 +44,7 @@ export function addToCart(productId){
     saveToLocalStorage();
 };
 
-//Remove a product from the cart when we press the delete button
+//Remove a product from the cart when we press the delete button, we are creating a new list to save the cart withoud including the deleting item and the save the data back to the cart, as we are modyfing the cart variable we then save to local storage.
 
 export function removeFromCart(productId){
     const newCart = [];
